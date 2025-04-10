c7187ba856ef253c5e7e73198facb771d5662d36
AB2 <126097559+AnmolB2@users.noreply.github.com>
2025-04-10 13:57:37 +0530

    JSNB File Uploaded to Git by User

--- concepts.js
+++ concepts.js
@@ -1,9 +1,20 @@
-const order = { id: 1024, status: "shipped", items: 3 };
-
-match (order) {
-  { status: "pending" } => console.log("Your order is being processed."),
-  { status: "shipped" } => console.log("Your order has been shipped."),
-  { status: "delivered" } => console.log("Your order has been delivered."),
-  { status } => console.log(`Order status: ${status}`),
-  _ => console.log("Unknown order status"),
+{
+  "metadata": {
+    "name": "concepts",
+    "language_info": {
+      "name": "JavaScipt",
+      "version": "8.0"
+    }
+  },
+  "jsnbversion": "v0.1",
+  "cells": [
+    {
+      "code": "const order = { id: 1024, status: \"shipped\", items: 3 };\n\nmatch (order) {\n  { status: \"pending\" } => console.log(\"Your order is being processed.\"),\n  { status: \"shipped\" } => console.log(\"Your order has been shipped.\"),\n  { status: \"delivered\" } => console.log(\"Your order has been delivered.\"),\n  { status } => console.log(`Order status: ${status}`),\n  _ => console.log(\"Unknown order status\"),\n}",
+      "status": "[-]",
+      "output": "<p class=\"error\">Unexpected token '{'</p>",
+      "type": "code"
+    }
+  ],
+  "source": "https://github.com/gopi-suvanam/scribbler",
+  "run_on_load": false
 }
